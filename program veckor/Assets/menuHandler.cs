024e2e7243bba57be5b622e1bf1191b83324bf3b
Nomiz-Crown <simon.andersen2@elev.ga.lbs.se>
2025-01-09 09:55:47 +0100

    menu start

---
+++ program veckor/Assets/menuHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuHandler : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject settingsButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
