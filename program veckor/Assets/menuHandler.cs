9b1d88755ba39aa05dc14258dd7b2b99a0473651
Nomiz-Crown <simon.andersen2@elev.ga.lbs.se>
2025-01-10 09:32:28 +0100

    menu

--- program veckor/Assets/menuHandler.cs
+++ program veckor/Assets/menuHandler.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menuHandler : MonoBehaviour
 {
     public GameObject playButton;
     public GameObject settingsButton;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject creditsButton;
+    public GameObject creditsPanel;
+
+    // Method to load a scene
+    public void LoadGameScene()
     {
-        
+        SceneManager.LoadScene("startcutscene");  // Replace with your target scene name
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowCredits()
     {
-        
+        creditsPanel.SetActive(true);
     }
 }
