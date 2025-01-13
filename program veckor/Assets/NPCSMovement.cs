896d659ffeb486b17b7ef2fbb38e52e19a248349
Overtal3 <buldosh08@gmail.com>
2025-01-13 10:36:04 +0100

    saker hände
    ok

---
+++ program veckor/Assets/NPCSMovement.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+public class NPCSMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float MovementChance;
+    float MovementDirection;
+    float MovementAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // Denna kod bestämmer OM NPCn ska röra på sig
+        Unity.Mathematics.Random rand = new Unity.Mathematics.Random();
+        float MovementChance = rand.NextInt(1, 2);
+
+        // Denna Kod bestämmer vilket håll NPCn ska röra på sig.
+        Unity.Mathematics.Random ran = new Unity.Mathematics.Random();
+        float MovementDirection = ran.NextInt(1, 4);
+        Unity.Mathematics.Random ra = new Unity.Mathematics.Random();
+        float MovementAmount = ra.NextInt(1, 4);
+        if (MovementDirection == 1)
+        {
+            rb.velocity = new Vector2(0, 5);
+        }
+        else if (MovementDirection == 2)
+        {
+            rb.velocity = new Vector2(5, 0);
+
+        }
+        else if (MovementDirection == 3)
+        {
+            rb.velocity= new Vector2(-5, 0);
+
+        }
+        else if (MovementDirection == 4)
+        {
+            rb.velocity= new Vector2(0, -5);
+        }
+
+            
+            
+   
+
+
+
+
+
+    }
+}
