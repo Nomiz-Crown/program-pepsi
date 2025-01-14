aa2212d76d25c06ff4b42aa3e62dfbb56df1ee94
Nomiz-Crown <simon.andersen2@elev.ga.lbs.se>
2025-01-14 14:47:17 +0100

    änkligen

--- program veckor/Assets/NPCSMovement.cs
+++ program veckor/Assets/NPCSMovement.cs
@@ -1,10 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
-
-=======
->>>>>>> 3b43d4a28f3bff5808cad040a1c5416b0fa1f01f
 
 public class NPCSMovement : MonoBehaviour
 {
@@ -17,20 +13,6 @@ public class NPCSMovement : MonoBehaviour
     float moveDistance = 2f; // Move distance in Unity units
 
     void Start()
-<<<<<<< HEAD
-=======
-=======
-    float MovementChance;
-    float MovementDirection;
-    float MovementAmount;
-    // Start is called before the first frame update
-<<<<<<< HEAD
-    void Start()
-=======
-     void Start()
->>>>>>> 132524fcbaad071f5a213db92f686b7f3bb89bfe
->>>>>>> 3b43d4a28f3bff5808cad040a1c5416b0fa1f01f
->>>>>>> e3c32d69697957e8f07862d32c29b46bd52ee025
     {
         rb = GetComponent<Rigidbody2D>();
         StartCoroutine(MoveRoutine());
@@ -38,73 +20,12 @@ public class NPCSMovement : MonoBehaviour
 
     IEnumerator MoveRoutine()
     {
-<<<<<<< HEAD
         while (true)
-=======
-<<<<<<< HEAD
-
-
-        // Denna kod bestämmer OM NPCn ska röra på sig
-        Unity.Mathematics.Random rand = new Unity.Mathematics.Random();
-        float MovementChance = rand.NextInt(1, 2);
-
-        // Denna Kod bestämmer vilket håll NPCn ska röra på sig.
-        Unity.Mathematics.Random ran = new Unity.Mathematics.Random();
-        float MovementDirection = ran.NextInt(1, 4);
-        // Denna kod bstämmer hur länge den kommer  röra på sig
-        Unity.Mathematics.Random ra = new Unity.Mathematics.Random();
-        float MovementAmount = ra.NextInt(1, 3);
-        if (MovementDirection == 1)
-=======
-<<<<<<< HEAD
-        while (true)
-=======
-        System.Random rand = new System.Random();
-            int Movement = rand.Next(1, 4);
-
-
-        if (Movement == 1)
->>>>>>> 132524fcbaad071f5a213db92f686b7f3bb89bfe
->>>>>>> 3b43d4a28f3bff5808cad040a1c5416b0fa1f01f
->>>>>>> e3c32d69697957e8f07862d32c29b46bd52ee025
         {
             ChooseNewDirection();
             yield return new WaitUntil(() => isMoving == false);
             yield return new WaitForSeconds(waitTime);
         }
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        else if (MovementDirection == 2)
-=======
-<<<<<<< HEAD
-=======
-        else if (Movement == 2)
->>>>>>> 3b43d4a28f3bff5808cad040a1c5416b0fa1f01f
-        {
-            rb.velocity = new Vector2(5, 0);
-
-        }
-        else if (MovementDirection == 3)
-        {
-            rb.velocity= new Vector2(-5, 0);
-
-        }
-        else if (MovementDirection == 4)
-        {
-            rb.velocity= new Vector2(0, -5);
-        }
-
-            
-            
-   
-
-
-
-
-
->>>>>>> 132524fcbaad071f5a213db92f686b7f3bb89bfe
->>>>>>> e3c32d69697957e8f07862d32c29b46bd52ee025
     }
 
     void ChooseNewDirection()
